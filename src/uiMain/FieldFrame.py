dfcad37fe410ed8ebcf5e456cd9261493b8c0e74
SantiagoOchoaC <159485469+SantiagoOchoaC@users.noreply.github.com>
2024-09-14 22:40:48 -0500

    Solución llamado y captura de datos

--- src/uiMain/FieldFrame.py
+++ src/uiMain/FieldFrame.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, ttk
 
 
 def alertWarn(title, message):
@@ -21,13 +21,13 @@ class FieldFrame(tk.Frame):
     @arg enviado es una funcion que se llamara cuando el formulario se envie
     """
 
-    def __init__(self, tituloCriterios, criterios, tituloValores, valores, habilitado, parent, enviado):
+    def __init__(self, tituloCriterios, criterios, tituloValores, valores, habilitado, parent, devolucionLlamado):
         import principal
         colors = principal.colors
         super().__init__(parent, bg=colors["background"], padx=10, pady=10)
         self.parent = parent
         self.criterios = criterios
-        self.enviado = enviado
+        self.devolucionLlamado = devolucionLlamado
         self.valores = valores
         self.habilitado = habilitado
         self.datos = {}
@@ -47,29 +47,35 @@ class FieldFrame(tk.Frame):
         label_titulo_criterio.grid(row=0, column=0, padx=5, pady=5)
         label_titulo_valores.grid(row=0, column=1, padx=5, pady=5)
 
-        # CREAR LA ENTRADA ENTRY PARA CADA CRITERIO
+ # CREAR LA ENTRADA ENTRY O COMBOBOX PARA CADA CRITERIO
         for index, criterio in enumerate(criterios):
             elementoCriterio = tk.Label(self, text=criterio, bg = colors["background"], font=("Century", 14, "bold"), fg = colors["text"])
             elementoCriterio.grid(row=index+1, column=0, padx=5, pady=5)
 
-            elementoInput = tk.Entry(self, font=("Century", 12)) # SE UTILIZA Entry
+            # Si el criterio es "Tipo de Programación", usar un Combobox
+            if tituloCriterios == "Opciones":
+                elementoInput = ttk.Combobox(self, font=("Century", 12), values=valores, state="readonly")
+                elementoInput.current(0)  # Mostrar la primera opción por defecto
+            else:
+                # Para otros criterios, usar el Entry habitual
+                elementoInput = tk.Entry(self, font=("Century", 12))
+
+                if valores is not None and index < len(valores):
+                    elementoInput.insert(0, valores[index])
+
+                if habilitado is not None and index < len(habilitado) and not habilitado[index]:
+                    elementoInput.configure(state=tk.DISABLED)
+
             elementoInput.grid(row=index+1, column=1, padx=5, pady=5)
 
-            if valores is not None and index < len(valores):
-                elementoInput.insert(0, valores[index])
-
-            if habilitado is not None and index < len(habilitado) and not habilitado[index]:
-                elementoInput.configure(state=tk.DISABLED)
-            
+            # ALMACENAR LOS DATOS Y SUS VALORES
             self.datos[criterio] = {
                 "elementos": (elementoCriterio, elementoInput),
                 "value": None
-                }
-            
+            }
 
-        
         # BOTONES PARA ENVIAR O LIMPIAR
-        botonEnviar = tk.Button(self, text="Enviar", font=("Century", 12), bg=colors["azul"], fg=colors["text"], relief="ridge", bd= 3, command=lambda: self.guardarDatos())
+        botonEnviar = tk.Button(self, text="Enviar", font=("Century", 12), bg=colors["azul"], fg=colors["text"], relief="ridge", bd= 3, command=lambda: self.guardarDatos(devolucionLlamado))
         botonEnviar.grid(row=len(criterios)+1, column=1, padx=5, pady=5)
 
         botonLimpiar = tk.Button(self, text="Limpiar", font=("Century", 12), bg=colors["naranja"], fg=colors["text"], relief="ridge", bd= 3, command=self.limpiarCasillas)
@@ -77,10 +83,14 @@ class FieldFrame(tk.Frame):
         
 
     # MÉTODOS DE LA CLASE
+
     def getValue(self, criterio):
+        """
+            Obtiene el valor de un criterio que se pasa como parametro. 
+        """
         return self.datos[criterio]["value"]
     
-    def guardarDatos(self):
+    def guardarDatos(self, devolucionLlamado): # AGREGAR EL ERROR SUGERIDO
         camposVacios = []
 
         for criterio in self.criterios:
@@ -90,16 +100,20 @@ class FieldFrame(tk.Frame):
 
             if (value == ""): # VERIFICAR SI HAY CAMPOS VACIOS 
                 camposVacios.append(criterio) # ALMACENA PARA INFORMAR CUAL CRITERIO FALTA
+
+            # Try
+            if (devolucionLlamado != None):
+                devolucionLlamado(self.formularioDatos)
         
         if (camposVacios):
             alertWarn("Entradas Vacias", f"Error, por favor completa todos los espacios. Faltan:{', '.join(camposVacios)}")
             return False
         
-        if self.enviado:
-            self.enviado(self.formularioDatos)
-        return True
     
     def limpiarCasillas(self):
+        """
+            Limpia todos los datos del formulario
+        """
         for criterio in self.criterios:
             self.datos[criterio]["elementos"][1].delete(0,"end")
 
